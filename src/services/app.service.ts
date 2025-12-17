07b85ffb23cddcb08708b99fb1f5d0e1c7bd315b
itstelha <itstelha@example.com>
2025-12-17 11:04:55 +0000

    apso commit

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const date = new Date().toLocaleString();
+    return `Hello World! Today is ${date}`;
   }
 }
